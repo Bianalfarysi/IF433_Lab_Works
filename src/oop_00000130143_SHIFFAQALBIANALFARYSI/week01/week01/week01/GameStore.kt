fde0f5ae337ed213c17e2e90a7090cc1300343d5
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-02-03 20:13:58 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week01/week01/week01/GameStore.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week01/week01/week01/GameStore.kt
@@ -4,4 +4,11 @@ package oop_00000130143_SHIFFAQALBIANALFARYSI.week01.week01.week01
 fun main() {
     val gameTitle = "Cyber Adventure"
     val price = 650000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 500000) price * 20 / 100
+    else price * 10 / 100
