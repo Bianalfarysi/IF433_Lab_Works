abb40ee5a9d48bb54b125cc33633cec9466cc50b
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-02-10 15:39:21 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week02/Main.kt
@@ -0,0 +1,26 @@
+package oop_00000130143_SHIFFAQALBIANALFARYSI.week02
+
+import  java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    println("Masukkan Nama: ")
+    val name = scanner.nextLine()
+    println("Masukkan Nim (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter")
+    } else {
+        println("Masukkan Jurusan: ")
+        val major = scanner.next()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai")
+    }
+}
