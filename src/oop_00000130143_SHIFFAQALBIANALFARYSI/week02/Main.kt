5fc2d320f4adec1cb19fdbceefb5078327d76c32
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-02-10 22:45:07 +0700

    week02: (task 1) implement library input system

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week02/Main.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week02/Main.kt
@@ -3,41 +3,33 @@ package oop_00000130143_SHIFFAQALBIANALFARYSI.week02
 import  java.util.Scanner
 
 fun main() {
+
     val scanner = Scanner(System.`in`)
 
-    println("--- APLIKASI PMB UMN ---")
+    println("=== LIBRARY FINE SYSTEM ===")
 
-    print("Masukkan Nama: ")
-    val name = scanner.nextLine()
+    print("Masukkan Judul Buku: ")
+    val title = scanner.nextLine()
 
-    print("Masukkan Nim (Wajib 5 Karakter): ")
-    val nim = scanner.next()
-    scanner.nextLine()
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
 
-    if (nim.length != 5) {
-        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter")
-    } else {
+    print("Masukkan Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
 
-        print("Pilih Jalur (1. Reguler, 2. Umum): ")
-        val type = scanner.nextInt()
-        scanner.nextLine()
-
-        if (type == 1) {
-            print("Masukkan Jurusan: ")
-            val major = scanner.nextLine()
-
-            val s1 = Student(name, nim, major)
-            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
-
-        } else if (type == 2) {
-
-            val s2 = Student(name, nim)
-            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
-
-        } else {
-            println("Pilihan ngawur, pendaftaran batal!")
-        }
+    // Validasi IF (tidak boleh minus)
+    if (duration < 0) {
+        duration = 1
     }
+
+    val loan = Loan(title, borrower, duration)
+
+    println("\n=== DETAIL PEMINJAMAN ===")
+    println("Judul Buku  : ${loan.bookTitle}")
+    println("Peminjam    : ${loan.borrower}")
+    println("Durasi      : ${loan.loanDuration} hari")
+    println("Total Denda : Rp ${loan.calculateFine()}")
 }
+
 
 
