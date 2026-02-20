63c7706dc894face2a43772f87302474b7418302
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-02-20 14:44:57 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week03/Employee.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week03/Employee.kt
@@ -1,2 +1,7 @@
 package oop_00000130143_SHIFFAQALBIANALFARYSI.week03
 
+var salary: Int = 0
+    set(value) {
+        println("Mencoba Set gaji ke: $value")
+        this.salary = value
+    }
---
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000130143_SHIFFAQALBIANALFARYSI.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("___ Test Erroe ___")
+    e.salary = 5000
+}
