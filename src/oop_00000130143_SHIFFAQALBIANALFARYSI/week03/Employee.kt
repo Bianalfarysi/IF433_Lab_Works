b1edf3e167c443e77084025d9f31b3e03a8ac63b
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-02-23 13:48:07 +0700

    week03: fix setter using backing field

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week03/Employee.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week03/Employee.kt
@@ -3,6 +3,11 @@ package oop_00000130143_SHIFFAQALBIANALFARYSI.week03
 class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
-            this.salary = value
+            if (value < 0) {
+                println("Salary tidak boleh negatif!")
+                field = 0
+            } else {
+                field = value
+            }
         }
 }
