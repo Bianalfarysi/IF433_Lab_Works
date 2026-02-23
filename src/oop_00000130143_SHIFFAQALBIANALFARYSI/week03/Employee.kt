ca1ebab4289a2b0dc594799c9bcd81dc392dba15
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-02-23 13:42:09 +0700

    week03: create basic Employee class

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week03/Employee.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week03/Employee.kt
@@ -1,15 +1,5 @@
 package oop_00000130143_SHIFFAQALBIANALFARYSI.week03
 
 class Employee(val name: String) {
-
     var salary: Int = 0
-        set(value) {
-            if (value < 0) {
-                println("ERROR : Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0
-            } else {
-                field = value
-            }
-        }
-
 }
