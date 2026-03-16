9c3820443cc789aaa99e2f50b4e8b9d5eb6bec55
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-03-16 20:04:07 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week06/Main.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week06/Main.kt
@@ -1,2 +1,21 @@
 package oop_00000130143_SHIFFAQALBIANALFARYSI.week06
 
+fun proccessCheckout(method: PaymentMethod, amount: Double) {
+    println("-> Memulai Checkout: ....")
+    method.pay(amount)
+}
+
+fun main(){
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = SmartPhone()
+    myPhone.turnOn()
+
+    val  pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== TESTING CHECKOUT ===")
+    proccessCheckout(method = pay1, amount = 50000.0)
+    proccessCheckout(method = pay2, amount = 150000.0)
+}
--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week06/PaymentMethod.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week06/PaymentMethod.kt
@@ -1,2 +1,6 @@
 package oop_00000130143_SHIFFAQALBIANALFARYSI.week06
 
+interface PaymentMethod {
+    fun pay(amount: Double)
+}
+
