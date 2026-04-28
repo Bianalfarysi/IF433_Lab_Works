2f8187f48ea1dd90be85bc3081bf6c3557a981fc
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-04-28 15:15:48 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week10/WalletModels.kt
@@ -0,0 +1,6 @@
+package oop_00000130143_SHIFFAQALBIANALFARYSI.week10
+
+// Model data untuk Crypto Wallet
+data class Coin(val name: String, val balance: Double)
+
+data class Transaction(val id: String, val amount: Double)
