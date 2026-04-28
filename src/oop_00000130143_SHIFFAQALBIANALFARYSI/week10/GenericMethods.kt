19eff51e04b1633a3ec8fb2cf92680c903b6b61e
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-04-28 15:11:28 +0700

    week10: implement generic function with return type

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week10/GenericMethods.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week10/GenericMethods.kt
@@ -1,2 +1,11 @@
 package oop_00000130143_SHIFFAQALBIANALFARYSI.week10
 
+// LANGKAH 1: Fungsi generik cetak data
+fun <T> printData(data: T) {
+    println("Data yang diterima: $data")
+}
+
+// LANGKAH 2: Fungsi generik dengan tipe kembalian
+fun <T> processData(input: T): T {
+    return input
+}
