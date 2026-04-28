7887452fb5573f0d011169f2167c3c32199049d2
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-04-28 15:23:19 +0700

    week10: (task) populate coin repository

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week10/CryptoDashboard.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week10/CryptoDashboard.kt
@@ -1,2 +1,9 @@
 package oop_00000130143_SHIFFAQALBIANALFARYSI.week10
 
+fun main() {
+    val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.85))
+    coinRepo.add(Coin("ETH", 12.5))
+    coinRepo.add(Coin("USDT", 500.0))
+}
