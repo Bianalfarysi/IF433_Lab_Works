af772553f153c2fd56377c7fc989814dbc57dffa
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-05-05 14:58:57 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week11/SmartHomeDashboard.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week11/SmartHomeDashboard.kt
@@ -1,2 +1,16 @@
 package oop_00000130143_SHIFFAQALBIANALFARYSI.week11
 
+fun main(){
+    println("=== SMART HOME CONFIGURATION PIPELINE ===")
+    val homeDevices = mutableListOf<SmartDevice>()
+
+    println("\n[SETUP] Mengonfigurasi perangkat pencahayaan...")
+    SmartDevice("Philips WiZ Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        println("(LOG) Lampu pintar berhasil dikonfigurasi: ${it.name}")
+        homeDevices.add(it)
+    }
+
+}
