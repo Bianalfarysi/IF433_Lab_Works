983c23cff9e1d302138966360b93449a99c1a593
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-05-19 15:20:09 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/oop_00000130143_SHIFFAQALBIANALFARYSI/week13/CryptoTradeLogger.kt
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/week13/CryptoTradeLogger.kt
@@ -59,4 +59,10 @@ fun main() {
 
     File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
     println("Baris data korup berhasil di-inject ke file CSV.")
+
+    println("\n=== MEMUAT DATA TRANSAKSI ===")
+    val loadedData = loadTrades("crypto_trades.csv")
+    val totalPnl = loadedData.sumOf { it.pnl }
     }
+
+
