a835e960b8acec9edd6c5902e3bfc46165c25f14
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-05-26 15:05:54 +0700

    week14: refactor SimplePrinter to implement only Printable

---
+++ src/oop_00000130143_SHIFFAQALBIANALFARYSI/Week14/ IspRefactored.kt
@@ -0,0 +1,11 @@
+interface Printable {
+    fun print(doc: String)
+}
+
+interface Scannable {
+    fun scan(doc: String): String
+}
+
+class ModernSimplePrinter : Printable {
+    override fun print(doc: String) = println("Printing securely: $doc")
+}
